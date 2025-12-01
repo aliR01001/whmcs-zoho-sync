83a1e3b3c0bdf5787dd1dfb5921d21477c609a36
ali rami <ali.rami@hostino.com>
2025-12-01 17:19:46 +0100

    integrate the cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: undefined, // Will use Cloudflare Pages adapter
+  adapter: cloudflare(), // Will use Cloudflare Pages adapter
 });
